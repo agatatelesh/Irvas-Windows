570f201626c54fb8d6ca87688789a2a304b3ced4
Agata <agatamagata91@gmail.com>
2018-11-03 13:31:06 +0300

    Форма

--- js/script.js
+++ js/script.js
@@ -44,7 +44,53 @@ window.addEventListener('DOMContentLoaded', function() {
         modalWindows(phoneLink,morePhone);
         modalWindows(headerBtn,more);
         
-        //Form     
-          
+        //Form   
+        let massage = {
+            loading: 'Идет отправка...',
+            success: 'Отправлено!',
+            failure: 'Произошла ошибка...'
+    
+        };
+        
+        let mainForm = document.querySelector('.main_form'),
+            mainInput = mainForm.getElementsByTagName('input'),
+            statusMessege = document.createElement('div');
+            console.log(mainForm);
+            console.log(mainInput);
+        
+            statusMessege.classList.add('status');
+
+            mainForm.addEventListener('submit', function(event) { 
+                event.preventDefault(); 
+                mainForm.appendChild(statusMessege); 
+
+                let request = new XMLHttpRequest();
+                request.open('POST', 'server.php');
+                request.setRequestHeader ('Content-Type', 'application/json; charset=utf-8');
+
+                let formData = new FormData(mainForm);let obj = {};
+                formData.forEach(function(value, key) {
+                    obj[key] = value;
+                });
+                let json = JSON.stringify(obj);
+        
+                request.send(json);
+
+                request.addEventListener('readystatechange', function() {
+                    if (request.readyState < 4) {
+                        statusMessege.innerHTML = massage.loading;
+                    } else if (request.readyState === 4 && request.status == 200) {
+                        statusMessege.innerHTML = massage.success;
+                    } else {
+                        statusMessege.innerHTML = massage.failure;
+                    }
+                });
+
+                for (let i = 0; i < mainInput.length; i++) {
+                    mainInput[i].value = '';
+                }
+            });
+
+           
     
 });
